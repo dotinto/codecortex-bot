7130577427ce15445968c31e9872e00caba323e2
dotinto <corriolgamer@gmail.com>
2023-04-11 22:13:02 +0300

    .

---
+++ commands/profile.ts
@@ -0,0 +1,59 @@
+const {
+	AttachmentBuilder,
+	SlashCommandBuilder
+} = require("discord.js")
+const Canvas = require("@napi-rs/canvas");
+const { request } = require("undici")
+var bgUrl = "https://raw.githubusercontent.com/discordjs/guide/main/guide/popular-topics/images/canvas.jpg";
+
+const applyText = (canvas, text) => {
+	const context = canvas.getContext('2d');
+
+	let fontSize = 70;
+
+	do {
+		context.font = `${fontSize -= 10}px sans-serif`;
+	} while (context.measureText(text).width > canvas.width - 300);
+	return context.font;
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName("profile")
+	.setDescription("Getting user's profile stats"),
+	async execute(interaction) {
+		var canvas = Canvas.createCanvas(700, 250);
+		var context = canvas.getContext("2d");
+		var background = await Canvas.loadImage(bgUrl);
+
+		context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+		context.strokeStyle = "#0099ff";
+		context.strokeRect(0, 0, canvas.width, canvas.height);
+
+		var {body} = await request(interaction.user.displayAvatarURL({extension: "jpg"}))
+		var avatar = await Canvas.loadImage(body.arrayBuffer())
+
+		context.drawImage(avatar, 25, 25, 200, 200);
+
+		context.beginPath();
+		context.arc(125, 125, 100, 0, Math.PI * 2, true);
+		context.closePath();
+		context.clip();
+
+		context.font = applyText(canvas, interaction.member.displayName);
+		context.fillStyle = '#ffffff';
+		context.fillText(interaction.member.displayName, canvas.width / 2.5, canvas.height / 1.8);
+
+		context.font = '28px sans-serif';
+		context.fillStyle = '#ffffff';
+		context.fillText('Profile', canvas.width / 2.5, canvas.height / 3.5);
+							
+		var attachment = new AttachmentBuilder(await canvas.encode("png"), {
+			name: "profile-image.png"
+		})
+
+		interaction.reply({files: [attachment]})
+		
+	}	
+}
---
+++ index.ts
@@ -0,0 +1,65 @@
+const {
+	Client,
+	GatewayIntentBits,
+	REST,
+	Routes,
+	Collection,
+	Events
+} = require("discord.js");
+const path = require("path")
+const fs = require("node:fs")
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const TOKEN = process.env.TOKEN || require("./config.js").TOKEN;
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+client.commands = new Collection();
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	const scommand = require(`./commands/${file}`);
+    commands.push(scommand.data.toJSON())
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		const data = await rest.put(
+			Routes.applicationCommands("1086602163750449194"),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+	
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+})
+
+client.login(TOKEN)
